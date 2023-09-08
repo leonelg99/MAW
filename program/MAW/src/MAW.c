456aaff928a32938ab38382979d7c14928bfde54
leonelg99 <lguerrico@alu.ing.unlp.edu.ar>
2023-09-07 22:52:07 -0300

    Well... lets go!!!!!

---
+++ program/MAW/src/MAW.c
@@ -0,0 +1,36 @@
+/*=============================================================================
+ * Program: MAW
+ * Date: 2023/09/07
+ * Version: 1
+ *===========================================================================*/
+
+/*=====[Inclusions of function dependencies]=================================*/
+
+#include "MAW.h"
+#include "sapi.h"
+#include "MSMotorShield.h"
+
+/*=====[Definition macros of private constants]==============================*/
+
+/*=====[Definitions of extern global variables]==============================*/
+
+/*=====[Definitions of public global variables]==============================*/
+
+/*=====[Definitions of private global variables]=============================*/
+
+/*=====[Main function, program entry point after power on or reset]==========*/
+
+int main( void )
+{
+   // ----- Setup -----------------------------------
+
+   // ----- Repeat for ever -------------------------
+   while( 1 ) {
+
+   }
+
+   // YOU NEVER REACH HERE, because this program runs directly or on a
+   // microcontroller and is not called by any Operating System, as in the 
+   // case of a PC program.
+   return 0;
+}
